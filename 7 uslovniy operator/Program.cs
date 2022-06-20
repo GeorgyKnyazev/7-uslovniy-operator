e29811c20b29337ec0b7a69dd01beb59e728ef93
Георгий Князев <saint_georgy@yahoo.com>
2022-06-20 13:52:13 +0300

    Добавьте файлы проекта.

---
+++ 7 uslovniy operator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _7_uslovniy_operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userTextToOutput;
+            char symbolForFraming;
+            int stringLight;
+
+            Console.Write("Введите текс для вывода: ");
+            userTextToOutput = Console.ReadLine();
+            Console.Write("введите символ для рамки: ");
+            symbolForFraming = Convert.ToChar(Console.ReadLine());
+            stringLight = userTextToOutput.Length;
+
+            for (int i = 0; i < (stringLight + 2); i++)
+            {
+                Console.Write(symbolForFraming);
+            }
+            Console.SetCursorPosition(0, 3);
+            Console.WriteLine(symbolForFraming + userTextToOutput + symbolForFraming);
+            for (int i = 0; i < (stringLight + 2); i++)
+            {
+                Console.Write(symbolForFraming);
+            }
+        }
+    }
+}
